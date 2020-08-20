6db5c0ee247bf45a951c1e79eab9af31890e280a
jsha129 <jaynish.shah@gmail.com>
2020-08-20 11:39:26 +1000

    addDashes and addHashes

---
+++ R/addDashes.R
@@ -0,0 +1,20 @@
+
+#' @title addDashes (for formatting code)
+#' @description Adds '### --' (50 '-') for making sections in code and general formatting. The function is called by Rstudio up on a hot key combinations.
+#' @return
+#' @export
+#'
+
+addDashes <- function(){
+  rstudioapi::insertText(paste0("### ", Reduce(paste0, rep("-",50))))
+}
+
+
+#' @title addHashes (for formatting code)
+#' @description Adds '###' (54 '#') for making sections in code and general formatting. The function is called by Rstudio up on a hot key combinations.
+#' @return
+#' @export
+#'
+addHashes <- function(){
+  rstudioapi::insertText(paste0("###", Reduce(paste0, rep("#",51))))
+}
